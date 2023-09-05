560b6b0440caa75422550d4e4bc7f356cf7121ae
moawezz <moawez.de@gmail.com>
2023-09-05 20:38:46 +0330

    add a query in list view shows task if creator is user

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Task
 
@@ -10,24 +12,38 @@ class TaskListView(generic.ListView):
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(is_complete=False).count()
+        return context
+
 
 class TaskDetailView(generic.DetailView):
     model = Task
     template_name = 'tasks/task_detail.html'
     context_object_name = 'task'
 
-
-class TaskCreateView(generic.CreateView):
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
     template_name = 'tasks/task_create.html'
     fields = '__all__'
 
-class TaskUpdateView(generic.UpdateView):
+
+class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin,  generic.UpdateView):
     model = Task
     template_name = 'tasks/task_update.html'
     fields = '__all__'
 
-class TaskDeleteView(generic.DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
+
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Task
     template_name = 'tasks/task_delete.html'
     success_url = reverse_lazy('tasks_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
